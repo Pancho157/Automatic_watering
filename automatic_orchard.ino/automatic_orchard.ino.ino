c8e910cbe5e9b2e69234b9e444c40fb15002528a
Pancho157 <hoxocraft65@gmail.com>
2023-03-17 06:47:44 -0300

    Added menu options, milis reset, buttons actions and watering conditions

--- automatic_orchard.ino/automatic_orchard.ino.ino
+++ automatic_orchard.ino/automatic_orchard.ino.ino
@@ -41,11 +41,20 @@ int sensor_2_START_WATERING_PERCENTAGE = 0;
 int sensor_1_TIME_WATERING = 0;
 int sensor_2_TIME_WATERING = 0;
 
-int sensor_1_TIME_BETWEEN_WATERING = 3600000 // 1 hr //
-int sensor_2_TIME_BETWEEN_WATERING = 3600000 // 1 hr //
+int sensor_1_TIME_BETWEEN_WATERING = 3600000    // 1 hr //
+  int sensor_2_TIME_BETWEEN_WATERING = 3600000  // 1 hr //
+
+  int zone_1_LAST_WATERING = 0;
+int zone_2_LAST_WATERING = 0;
 
 int rele_1_STATUS = 0;
 int rele_2_STATUS = 0;
+
+int last_watering_zone_1 = 0 int last_watering_zone_2 = 0
+
+  // Used to set milis to 0
+  extern volatile unsigned long timer0_millis;
+unsigned long timer_new_value = 0;
 
 // -------------------------------------------------------------
 
@@ -75,11 +84,19 @@ void loop() {
   sensor_1_START_WATERING_PERCENTAGE = map(sensor_1_START_WATERING_VALUE, dirt_sensor_1_BOTTOM_VALUE, dirt_sensor_1_TOP_VALUE, 100, 0);
   sensor_2_START_WATERING_PERCENTAGE = map(sensor_2_START_WATERING_VALUE, dirt_sensor_2_BOTTOM_VALUE, dirt_sensor_2_TOP_VALUE, 100, 0);
 
+  if (actual_time > interval_to_set_time_to_0 && rele_1_STATUS == 0 && rele_2_STATUS == 0) {
+    setMillis(timer_new_value);  // coloca los millis en 0
+
+    actual_time = millis();
+    zone_1_LAST_WATERING = 0;
+    zone_2_LAST_WATERING = 0;
+  }
+
   if (nextButton == HIGH) {
     while (nextButton == HIGH) {}
     menu_index++;
     if (menu_index > 5) {
-      menu_index = 0;
+      menu_index = 0;  // Menu bottom
     }
   }
 
@@ -87,7 +104,7 @@ void loop() {
     while (backButton == HIGH) {}
     menu_index--;
     if (menu_index < 0) {
-      menu_index = 5;
+      menu_index = 5;  // Menu top
     }
   }
 
@@ -102,16 +119,46 @@ void loop() {
 void menu() {
   switch (menu_index) {
     case 0;
+      // Sensors humidity display
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Lectura suelo 1");
-      lcd.setCursor(0, 1);
-      lcd.print("Valor:");
-      lcd.setCursor(8, 1);
-      lcd.print(lecturaSueloUno);
-      lcd.setCursor(11, 1);
+      lcd.print("Suelo 1:");
+      lcd.setCursor(10, 0);
       lcd.print(porcentajeSueloUno);
-      lcd.setCursor(14, 1);
+      lcd.print("%");
+      lcd.setCursor(0, 1);
+      lcd.print("Suelo 2:");
+      lcd.setCursor(10, 1);
+      lcd.print(porcentajeSueloUno);
+      lcd.print("%");
+      break;
+  }
+    case 1;
+      // Sensors humidity display
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("> Inicio Z1 +3%");
+      lcd.setCursor(10, 0);
+      lcd.print(porcentajeSueloUno);
+      lcd.print("%");
+      lcd.setCursor(0, 1);
+      lcd.print("Suelo 2:");
+      lcd.setCursor(10, 1);
+      lcd.print(porcentajeSueloUno);
+      lcd.print("%");
+      break;
+    case 2;
+      // Sensors humidity display
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Inicio Z1 +3%");
+      lcd.setCursor(10, 0);
+      lcd.print(porcentajeSueloUno);
+      lcd.print("%");
+      lcd.setCursor(0, 1);
+      lcd.print("> Suelo 2:");
+      lcd.setCursor(10, 1);
+      lcd.print(porcentajeSueloUno);
       lcd.print("%");
       break;
   }
@@ -120,3 +167,34 @@ void menu() {
 // -------------------------------------------------------------
 
 void execute_function() {}
+
+void verify_watering_conditions() {
+  if (tiempo - ultimoRiegoZ1 >= tiempoEntreRiegosZ1 && valorInicialSueloUno > lecturaSueloUno) {
+    estadoReleZ1 = 1;
+    digitalWrite(releSueloUno, estadoReleZ1)
+      ultimoRiegoZ1 = tiempo;
+  }
+
+  if (tiempo - ultimoRiegoZ2 >= tiempoEntreRiegosZ2 && valorInicialSueloDos > lecturaSueloDos) {
+    estadoReleZ2 = 1;
+    digitalWrite(releSueloDos, estadoReleZ2)
+      ultimoRiegoZ2 = tiempo;
+  }
+
+  if (tiempo - ultimoRiegoZ1 >= estadoReleZ1) {
+    estadoReleZ1 = 0;
+    digitalWrite(releSueloUno, LOW)
+  }
+
+  if (tiempo - ultimoRiegoZ2 >= tiempoDeRiegoDos) {
+    estadoReleZ2 = 0;
+    digitalWrite(releSueloDos, estadoReleZ2)
+  }
+}
+
+void setMillis(unsigned long new_millis) {
+  uint8_t oldSREG = SREG;
+  cli();
+  timer0_millis = new_millis;
+  SREG = oldSREG;
+}
